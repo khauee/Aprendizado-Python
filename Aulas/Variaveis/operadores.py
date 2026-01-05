93dc2674e560f857e7de6e2b6718788b19c1b7f0
khauee <khauebraga55@gmail.com>
2026-01-05 11:30:32 -0300

    Testando variaveis e operadores

---
+++ Aulas/Variaveis/operadores.py
@@ -0,0 +1,26 @@
+a = 2 
+b = 5
+c = 3
+
+print(a+b)#soma
+print(a-b)#subtração
+print(a*b)#multiplicação
+print(a/b)#divisão
+
+print(a%b)#resto
+print(a**b)#Exponenciação
+print(a//c)#Divisão inteira
+
+#Operadores de comparação
+
+print(a==b)#igual
+print(a!=b)#diferente
+print(a>b)#maior
+print(a<b)#menor
+print(a>=b)#maior ou igual
+print(a<=b)#menor ou igual
+
+#Operadores lógicos
+print(a>b and b<c) #e -> retorna true se a condição for verdade
+print(a>b or c<a) #ou -> retorna true se alguma condição satisfazer
+print(not (a==c)) #inverte resultado da comparação
